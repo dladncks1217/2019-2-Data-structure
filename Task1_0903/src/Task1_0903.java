42992e361bafb0da35cd7c8b77575a9ea7268bc3
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-09-03 19:12:58 +0900

    0903

---
+++ Task1_0903/src/Task1_0903.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Number2{
+	private int i;
+	private int result1 = 0;
+	private int result2 = 0;
+	private int result2_2 = 0;
+	
+	Number2(int i){
+		this.i = i;
+	}
+	void number2_1(int a) {
+		long start = System.currentTimeMillis();
+		for(int k = 0;k<a;k++) {
+			result1 += a;
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("1번문제 답 "+result1);
+		System.out.println( "실행 시간 : " + ( end - start )/1000.000000000000 );
+		
+	}
+	void number2_2(int a) {
+		long start = System.currentTimeMillis();
+		for(int j = 0;j<a;j++) {
+			result2 +=1;
+		} 
+		result2_2 = result2;
+		for(int k = 0;k<result2_2;k++) {
+			result2+=result2_2;
+		}
+		long end = System.currentTimeMillis();
+		System.out.println("2번문제 답 "+result2);
+		System.out.println( "실행 시간 : " + ( end - start )/1000.0000000 );
+	}
+}
+public class Task1_0903 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		Number2 num = new Number2(a);
+		num.number2_1(a);
+		num.number2_2(a);
+		}
+}
---
+++ Task2_0903/src/Task2_0903.java
@@ -0,0 +1,9 @@
+
+public class Task2_0903 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
