1bfdab2efdcee2d357ab4dc4af3af9046a8c3679
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-09-03 20:06:49 +0900

    0903

--- Task2_0903/src/Task2_0903.java
+++ Task2_0903/src/Task2_0903.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
 
+class Number3{
+
+
+	void Task1(int a) {
+		if(a<=1) {
+			System.out.println(a-1);
+			return;
+		}
+		int []Arrayfor = new int[a];
+		Arrayfor[0] = 0;
+		Arrayfor[1] = 1;
+		for(int i = 2;i<a;i++) {
+			Arrayfor[i] = Arrayfor[i-1] + Arrayfor[i-2];
+		}
+		System.out.println("반복 이용입니다."+Arrayfor[a-1]);
+	}
+	int Task2(int a) {
+		if(a<=2)
+			return a-1;
+		else 
+			return Task2(a-1)+Task2(a-2);
+	}
+}
 public class Task2_0903 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		Number3 num = new Number3();
+		
+		num.Task1(a);
+		System.out.println("순환 이용입니다."+num.Task2(a));
 	}
 
 }
